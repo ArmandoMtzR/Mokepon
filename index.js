bc260ecbe7762be22bc89f02d9b53ac2cbb1b89f
Armando <h.armando.mtz@outlook.com>
2024-07-24 10:52:42 -0600

    Se agrego nodeModules para funcionamiento de servidor

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hola node")
+//Se importo express
+const express = require("express")
+//Creacion de app
+const app = express() 
+
+const jugadores = []
+
+class Jugador{
+    constructor(id) {
+        this.id = id
+    }
+}
+
+//Cuando se crea una peticion 
+app.get("/unirse", (req, res) => {
+    const id = `${Math.random()}`
+
+    const jugador = new Jugador(id)
+    jugadores.push(jugador)
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+
+    res.send(id) 
+})
+//Aqui se mantiene escuchando el servidor
+app.listen(8080, () => {
+    console.log("Servidor funcionando") 
+})
