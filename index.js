d28c71e4177edc7b3a1cbde5f6fc93184c6f3547
Armando <h.armando.mtz@outlook.com>
2024-07-30 20:52:32 -0600

    Se levanta el servidor con express, se asigna un moquepon y ID a cada jugador

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 //Se importo express
-const express = require("express")
+const express = require('express')
+const cors = require('cors')
 //Creacion de app
 const app = express() 
+
+app.use(cors())
+app.use(express.json())
 
 const jugadores = []
 
 class Jugador{
     constructor(id) {
         this.id = id
+    }
+    asignarMokepon(mokepon){
+        this.mokepon = mokepon
+    }
+}
+
+class Mokepon {
+    constructor(nombre){
+        this.nombre = nombre
     }
 }
 
@@ -23,6 +36,23 @@ app.get("/unirse", (req, res) => {
     res.send(id) 
 })
 //Aqui se mantiene escuchando el servidor
+
+app.post("/mokepon/:jugadorId", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const nombre = req.body.mokepon || ""
+    const mokepon = new Mokepon(nombre)
+
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+    if(jugadorIndex >= 0) {
+        jugadores[jugadorIndex].asignarMokepon(mokepon)
+    }
+
+    console.log(jugadores)
+    console.log(jugadorId)
+    res.end()
+})
+
 app.listen(8080, () => {
     console.log("Servidor funcionando") 
 })
